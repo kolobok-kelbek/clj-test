c5fd8d53525d83d77c42ffa6bb17381a89e24336
kelbek <dev_kelbek@mail.ru>
2020-05-13 11:16:29 +0300

    [backend] clean dependencies (delete http-kit, sort, move test dependencies from common to test envirinment)

--- back/api/project.clj
+++ back/api/project.clj
@@ -4,17 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [compojure "1.6.1"]
-                 [http-kit "2.2.0"]
-                 [metosin/compojure-api "1.1.11"]
-                 [ring/ring-json "0.4.0"]
-                 [ring/ring-defaults "0.3.1"]
-                 [cheshire "5.10.0"]
-                 [ring/ring-defaults "0.3.2"]
-                 [faker "0.2.2"]
-                 [re-rand "0.1.0"]
                  [org.clojure/data.generators "1.0.0"]                 
                  [org.clojure/java.jdbc "0.7.1"]
+                 [compojure "1.6.1"]
+                 [metosin/compojure-api "1.1.11"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [cheshire "5.10.0"]
                  [honeysql "0.9.10"]
                  [nilenso/honeysql-postgres "0.2.6"]
                  [org.flywaydb/flyway-core "6.4.0"]
@@ -27,7 +23,8 @@
   :ring {:handler clj-test.handler/app}
   
   :profiles { :test {:resource-paths ["resources/test"]
-                    }
+                     :dependencies [[faker "0.2.2"]
+                                    [re-rand "0.1.0"]]}
               :dev
                     {:resource-paths ["resources/dev"]
                      :dependencies [[javax.servlet/servlet-api "2.5"]
